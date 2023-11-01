3a3dc1574c94afa17005e60ab938823ebf73929e
danielriggi <daniel.l.riggi@gmail.com>
2023-11-01 13:26:41 -0400

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,8 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+		int yob = 1989;
+		String month = "March";
+		Boolean weekend = false;
+		System.out.println(yob + "\n" + month + "\n" + weekend);
+	}
+}
