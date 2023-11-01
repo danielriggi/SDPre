542cb64c75bc8dd9e0c9aed4fbe41f76154b11c2
danielriggi <daniel.l.riggi@gmail.com>
2023-11-01 13:51:19 -0400

    complete Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,9 @@
+public class Objective3Lab2 {
+	public static void main(String[] args) {
+		double side1 = 3;
+		double side2 = 4;
+		double hypotenuse = Math.sqrt(Math.pow(side1, 2) + Math.pow(side2,2));
+		System.out.println("The hypotenuse of a triangle with sides " + side1 + " and " + side2 + " is " + hypotenuse);
+
+	}
+}
