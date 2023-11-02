1a0246e9aa61175e4ac50f586424c64814bbf98b
danielriggi <daniel.l.riggi@gmail.com>
2023-11-02 15:24:57 -0400

    complete Objective9Lab1

---
+++ Labs/Objective9Lab1.java
@@ -0,0 +1,10 @@
+public class Objective9Lab1 {
+	public static void main(String[] args) {
+		Objective9Lab1.printName();
+
+	}
+
+	public static void printName() {
+		System.out.println("Hello Dan");
+	}
+}
